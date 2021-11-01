be72b1cb7bcbca0df2b70eb4e66aab7559f6122c
狙击美佐 <3058852685@qq.com>
2021-11-01 20:01:36 +0800

    Update 01_快速幂.cpp

--- 06_数学/01_快速幂.cpp
+++ 06_数学/01_快速幂.cpp
@@ -30,34 +30,45 @@ ll quick_mul(ll a,ll b,ll c) {
     return (a * b - (ll)((ld)a * b / c) * c + c) % c;
 }
 
+// 矩阵快速幂
 
-const int N = 100 + 10;
+struct Matrix {
+  ll m[3][3];
 
-int n;
+  void clear() { memset(m, 0, sizeof(m)); }
+  void init()  { for(int i = 1;i <= 2; i++) m[i][i] = 1; }
+  bool check() { return m[1][1] && !m[1][2] && !m[2][1] && m[2][2] ? 1 : 0; }
 
-struct Martix {
-    ll a[N][N];
-    Martix operator * (const Martix &rhs) const {
-        Martix ans;
-        mem(ans.a, 0);
-        for(int i = 1;i <= n; i++) {
-            for(int j = 1;j <= n; j++) {
-                for(int k = 1;k <= n; k++) {
-                    ans.a[i][j] = (ans.a[i][j] + a[i][k] * rhs.a[k][j]) % mod;
-                }
-            }
+  Matrix operator * (const Matrix &a) {
+    Matrix res; res.clear();
+    for(int i = 1;i <= 2; i++) {
+      for(int j = 1;j <= 2; j++) {
+        for(int k = 1;k <= 2; k++) {
+          (res.m[i][j] += a.m[i][k] * m[k][j]) %= mod; 
         }
-        return ans;
+      }  
     }
+    return res;
+  }
+
+  Matrix operator + (const Matrix &a) {
+    Matrix res; res.clear();
+    for(int i = 1;i <= 2; i++) {
+      for(int j = 1;j <= 2; j++) {
+        res.m[i][j] = (a.m[i][j] + m[i][j]) % mod;
+      }
+    }
+    return res;
+  }
 };
 
-Martix quick_pow(Martix a, ll b) {
-    Martix ans; mem(ans.a, 0);
-    for(int i = 1;i <= n; i++) ans.a[i][i] = 1;
-    while(b) {
-        if(b & 1) ans = a * ans;
-        a = a * a;
-        b >>= 1;
-    }
-    return ans;
+Matrix operator ^ (Matrix a, ll b) {
+  Matrix ans;
+  ans.clear(); ans.init();
+  while(b) {
+    if(b & 1) ans = ans * a;
+    a = a * a;
+    b >>= 1;
+  }
+  return ans;
 }
