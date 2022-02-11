ab84dfc90a7e28aa3092a189e70d410ea68f27d2
Zetao Feng <fengzetao.zed@foxmail.com>
2022-02-12 00:49:32 +0800

    Update 09_SPFA.cpp

--- 07_图论/09_SPFA.cpp
+++ 07_图论/09_SPFA.cpp
@@ -1,35 +1,35 @@
-#define INF 0x3f
-#define maxn 5000005
-int n, m, s;
+template<typename T>
+class SPFA {
+public :
+    const int n;
+    std::vector<std::vector<std::pair<int, T>>> edge;
+    std::vector<bool> vis;
+    std::vector<T> dis;
+    std::vector<int> cnt;
+    SPFA(int n) : n(n), edge(n), vis(n), dis(n, 1E9), cnt(n) {}
 
-struct Edge {
-    int v, w, next;
-}e[maxn];
-int head[maxn], cnt, dis[maxn];
-bool vis[maxn]; 
-
-inline void add(int u,int v,int w) ;
-
-queue<int> q;
-
-void SPFA() {
-    for(int i = 1;i <= n; i++) {
-        dis[i] = INF;
-        vis[i] = false;
+    void add(int u, int v, T w) {
+        edge[u].emplace_back(make_pair(v, w));
     }
-    q.push(s); dis[s] = 0; vis[s] = true;
-    while(!q.empty()) {
-        int u = q.front(); q.pop();
-        vis[u] = false;
-        for(int i = head[u]; i; i = e[i].next) {
-            int v = e[i].v;
-            if(dis[v] > dis[u] + e[i].w) {
-                dis[v] = dis[u] + e[i].w;
-                if(!vis[v]) {
-                    vis[v] = true;
-                    q.push(v);
+    bool spfa(int s) {
+        queue<int> q;
+        q.push(s);
+        dis[s] = 0, vis[s] = true;
+        while(!q.empty()) {
+            int u = q.front(); q.pop();
+            vis[u] = false;
+            for(auto e : edge[u]) {
+                if(dis[e.first] > dis[u] + e.second) {
+                    dis[e.first] = dis[u] + e.second;
+                    if(!vis[e.first]) {
+                        cnt[e.first]++;
+                        vis[e.first] = true;
+                        if(cnt[e.first] >= n) return false;
+                        q.push(e.first);
+                    }
                 }
             }
         }
+        return true;
     }
-}
+};
