2803958b692b3533fb991eceae70b6ab9d3ecb4d
Zetao Feng <fengzetao.zed@foxmail.com>
2022-02-12 00:49:13 +0800

    Update 11_prim.cpp

--- 07_图论/11_prim.cpp
+++ 07_图论/11_prim.cpp
@@ -1,48 +1,34 @@
-//prim算法求最小生成树（稠密图）
-
-struct Prim {
-    static const int N = 1e6 + 10;
-    int n, m;
-    double ans;
-    struct Edge {
-        int v, next;
-        double w;
-    }e[N << 1];
-    int head[N << 1], cnt;
-   struct node {
+template<typename T>
+class Prim {
+    const int n;
+    T ans;
+    std::vector<std::vector<std::pair<int, T>>> edge;
+    std::vector<bool> vis;
+    std::vector<T> dis;
+    struct node {
        int now;
-       double d;
+       T d;
        bool operator < (const node& rhs) const {
            return d > rhs.d;
        }
-   };
+    };
+    Prim(int n) : ans(0), n(n), edge(n), vis(n), dis(n, 1E9) {}
 
-   void init() {
-       memset(head, -1, sizeof head);
-       cnt = ans = 0;
-   }
-
-   inline void add(int u, int v, double w) {
-       e[cnt].v = v;
-       e[cnt].w = w;
-       e[cnt].next = head[u];
-       head[u] = cnt++;
-   }
-
-   void prim() {
-       vector<int> vis(n + 1);
-       priority_queue<node> q; q.push({1, 0});
-       int res = 0;
-       while(!q.empty() && res <= n) {
-           node p = q.top(); q.pop();
-           int u = p.now;
-           double dis = p.d;
-           if(vis[u]) continue;
-           vis[u] = 1, res++, ans += dis;
-           for(int i = head[u]; ~i; i = e[i].next) {
-               int v = e[i].v;
-               if(!vis[v]) q.push({v, e[i].w});
-           }
-       }
-   }
-}pr;
+    void add(int u, int v, T w) {
+        edge[u].emplace_back(make_pair(v, w));
+    }
+    void prim() {
+        priority_queue<node> q; q.push({0, 0});
+        int cnt = 0;
+        while(!q.empty() && cnt <= n) {
+            node p = q.top(); q.pop();
+            int u = p.now;
+            T dis = p.d;
+            if(vis[u]) continue ;
+            vis[u] = true, cnt++, ans += dis;
+            for(auto e : edge[u]) {
+                if(!vis[e.first]) q.push({e.first, e.second});
+            }
+        }
+    }
+};
