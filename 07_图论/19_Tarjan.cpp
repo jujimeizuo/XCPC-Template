b4bca082aed174103d99f4c5f3acd37e34780f6f
Zetao Feng <fengzetao.zed@foxmail.com>
2022-02-12 00:48:38 +0800

    Update 19_Tarjan.cpp

--- 07_图论/19_Tarjan.cpp
+++ 07_图论/19_Tarjan.cpp
@@ -1,69 +1,41 @@
-
-#include <bits/stdc++.h>
-
-using namespace std;
-
-const int N = 2e5 + 10;
-
-struct Edge{
-    int u, v, next;
-}e[N * 2];
-
-int head[N], cnt;
-
-int dfn[N], low[N]; // 第一次访问到的节点的编号和最早访问的节点的编号（时间戳）
-bool vis[N]; // 标记节点是否访问过并且还在栈中
-int Time = 1;
-stack<int> s;
-
-void add(int u, int v) {
-    e[++cnt].v = v;
-    e[cnt].next = head[u];
-    head[u] = cnt;
-}
-
-void DFS(int u) {
-    s.push(u);
-    dfn[u] = low[u] = Time++;
-    vis[u] = 1;
-
-    for(int i = head[u]; i; i = e[i].next) {
-        int v = e[i].v;
-        if(!dfn[v]) {
-            DFS(v);
-            low[u] = min(low[u], low[v]); // 更新时间戳
-        }
-        else if(vis[v]) {
-            low[u] = min(low[u], low[v]); // 更新时间戳
-        }
-
-    }
-
-    if(dfn[u] == low[u]) { // 找到了一个强连通分量
-        int x;
-        do {
-            x = s.top();
-            cout << x << " ";
-            vis[x] = 0;
-            s.pop();
-        }while(x != u);
-        cout << endl;
-    }
-}
-
-void Tarjan() {
-    int n, m;
-    cin >> n >> m;
-
-    for(int i = 1;i <= m; i++) {
-        int u, v;
-        cin >> u >> v;
-        add(u, v);
-    }
+class Tarjan {
+public :
+    long long ans;
+    const int n;
+    int tim;
+    vector<vector<int>> edge;
+    vector<int> dfn, low;
+    vector<bool> vis;
+    stack<int> s;
+    Tarjan(int n) : n(n), edge(n), dfn(n), low(n), vis(n), tim(1), ans(0) {}
     
-    for(int i = 1;i <= n; i++) {
-        if(!dfn[i])
-            DFS(i);
+    void add(int u, int v) {
+        edge[u].emplace_back(v);
     }
-}
-
+    void dfs(int u) {
+        s.push(u);
+        dfn[u] = low[u] = tim++;
+        vis[u] = true;
+        for(int v : edge[u]) {
+            if(!dfn[v]) {
+                dfs(v);
+                low[u] = min(low[u], low[v]);
+            }
+            else if(vis[v]) {
+                low[u] = min(low[u], low[v]);
+            }
+        }
+        // 找到一个联通分量
+        if(dfn[u] == low[u]) {
+            int from, cnt = 0;
+            do {
+                from = s.top();
+                vis[from] = false;
+                s.pop();
+                cnt++;
+                // TODO ... num = cnt
+            }while(from != u);
+        }
+    }
+    // for(int i = 1;i <= n; i++) if(!dfn[i]) dfs(i);
+};
