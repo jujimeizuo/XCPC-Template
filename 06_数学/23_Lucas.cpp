10867fd25de5dbcc873bad076dd38d5dc73a274e
Zetao Feng <fengzetao.zed@foxmail.com>
2022-02-13 10:25:52 +0800

    Update 23_Lucas.cpp

--- 06_数学/23_Lucas.cpp
+++ 06_数学/23_Lucas.cpp
@@ -1,26 +1,18 @@
-namespace Comb {
-    const int N = 1e6 + 10;
-    ll F[N], invF[N], inv[N];
-
-    void init() {
-        F[0] = F[1] = invF[0] = invF[1] = inv[0] = inv[1] = 1;
-        for (int i = 2; i < N; i++) {
-            F[i] = F[i - 1] * i % mod;
-            inv[i] = (mod - mod / i) * inv[mod % i] % mod;
-            invF[i] = invF[i - 1] * inv[i] % mod;
+class Comb {
+public :
+    const int n;;
+    std::vector<Z> fac, inv, ifac;
+    Comb(int n) : n(n), fac(n), inv(n), ifac(n) {
+        fac[0] = fac[1] = inv[0] = inv[1] = ifac[0] = ifac[1] = 1;
+        for(int i = 2;i < n; i++) {
+            fac[i] = fac[i - 1] * i;
+            inv[i] = (P - P / i) * inv[P % i];
+            ifac[i] = ifac[i - 1] * inv[i];
         }
     }
-
-    ll C(ll m, ll n) {
-        if (m < 0 || n < 0 || n > m) return 0;
-        ll ans = F[m];
-        ans = ans * invF[n] % mod;
-        ans = ans * invF[m - n] % mod;
-        return ans;
+    Z C(int n, int m) {
+        if(m < 0 || n < 0 || m > n) return 0;
+        return fac[n] * ifac[m] * ifac[n - m];
     }
-
-    ll Lucas(ll m, ll n) {
-        return n ? Lucas(m / mod, n / mod) * C(m % mod, n % mod) % mod : 1;
-    }
-
-}
+    Z Lucas(long long m, long long n) { return n ? Lucas(m / P, n / P) * C(m % P, n % P) : 1;}
+};
