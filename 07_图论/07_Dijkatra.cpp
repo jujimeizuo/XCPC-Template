a82faf9c999aac9fee98a366e8c6a64e7c764201
Zetao Feng <fengzetao.zed@foxmail.com>
2022-03-16 22:21:06 +0800

    Update 07_Dijkatra.cpp

--- 07_图论/07_Dijkatra.cpp
+++ 07_图论/07_Dijkatra.cpp
@@ -1,40 +1,38 @@
-const int maxn = 2e5 + 10;
+template<typename T>
+class Dijkstra {
+public :
+    const int n;
+    std::vector<std::vector<std::pair<int, T>>> edge;
+    std::vector<T> dis;
+    std::vector<bool> vis;
 
-struct Edge {
-    int v, next;
-    ll w;
-}e[maxn];
-int head[maxn], cnt;
+    struct node {
+        int now; T d;
+        bool operator < (const node &rhs) const {
+            return d > rhs.d;
+        }
+    };
 
-inline void add(int u, int v, ll w) ;
-
-struct node {
-    int now;
-    ll d;
-    bool operator < (const node &rhs) const {
-        return d > rhs.d;
+    Dijkstra(int n) : n(n), edge(n), vis(n), dis(n, 1E9) {}
+    void add(int u, int v, T w) {
+        edge[u].emplace_back(make_pair(v, w));
     }
-};
-
-ll dis[maxn];
-bool vis[maxn];
-priority_queue<node> q;
-
-void dij(int s) {
-    dis[s] = 0;
-    q.push({s, 0});
-    while(!q.empty()) {
-        node p = q.top();
-        q.pop();
-        int u = p.now;
-        if(vis[u]) continue;
-        vis[u] = 1;
-        for(int i = head[u]; i; i = e[i].next) {
-            int v = e[i].v;
-            if(dis[v] > dis[u] + e[i].w) {
-                dis[v] = dis[u] + e[i].w;
-                q.push({v, dis[v]});
+    void Dij(int s) {
+        priority_queue<node> q;
+        dis[s] = 0;
+        q.push({s, 0});
+        while(!q.empty()) {
+            node p = q.top();
+            q.pop();
+            int u = p.now;
+            if(vis[u]) continue;
+            vis[u] = 1;
+            for(auto e : edge[u]) {
+                if(dis[e.first] > dis[u] + e.second) {
+                    dis[e.first] = dis[u] + e.second;
+                    q.push({e.first, dis[e.first]});
+                }
             }
         }
     }
-}
+};
