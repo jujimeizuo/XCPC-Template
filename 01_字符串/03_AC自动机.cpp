7c5841915c3e8d94fa9ff52ccf76b5b0d7092312
Zetao Feng <fengzetao.zed@foxmail.com>
2022-04-02 11:49:18 +0800

    Create 03_AC自动机.cpp

--- 01_字符串/03_AC自动机.cpp
+++ 01_字符串/03_AC自动机.cpp
@@ -1,20 +1,18 @@
 class Automaton {
 public :
-    const int n;
     std::vector<std::vector<int>> tr;
     std::vector<int> e;
     std::vector<int> fail;
     int tot;
-
-    Automaton(int _n) : n(_n), tr(n, std::vector<int>(26)), e(n), fail(n), tot(0) {}
-    Automaton(int m, std::vector<string> s) : Automaton(m) {
+    Automaton(int n) : tot(0), tr(n, std::vector<int>(26)), e(n), fail(n) {}
+    Automaton(int m, std::vector<std::string> s) : Automaton(m) {
         for(int i = 0;i < (int) s.size(); i++) {
             insert(s[i]);
         }
         build();
     }
 
-    void insert(string s) {
+    void insert(std::string s) {
         int u = 0;
         for(int i = 0;i < (int) s.size(); i++) {
             if(!tr[u][s[i] - 'a']) tr[u][s[i] - 'a'] = ++tot;
@@ -24,11 +22,11 @@ public :
     }
 
     void build() {
-        queue<int> q;
+        std::queue<int> q;
         for(int i = 0;i < 26; i++) {
-            if(tr[0][i]) q.push(tr[0][i]);        
+            if(tr[0][i]) q.push(tr[0][i]);
         }
-        while(q.size()) {
+        while(!q.empty()) {
             int u = q.front();
             q.pop();
             for(int i = 0;i < 26; i++) {
@@ -42,14 +40,14 @@ public :
         }
     }
 
-    int query(string t) {
-        int u = 0, res = 0;
+    int query(std::string t) {
+        int u = 0, ans = 0;
         for(int i = 0;i < (int) t.size(); i++) {
             u = tr[u][t[i] - 'a'];
             for(int j = u;j && e[j] != -1; j = fail[j]) {
-                res += e[j], e[j] = -1;
+                ans += e[j], e[j] = -1;
             }
         }
-        return res;
+        return ans;
     }
 };
