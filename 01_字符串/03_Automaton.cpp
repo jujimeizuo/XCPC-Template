e6a03b99116f052c233e3b9dc24ddea547c0f4e0
Zetao Feng <fengzetao.zed@foxmail.com>
2022-09-27 19:08:58 +0800

    Update and rename 03_AC自动机.cpp to 03_Automaton.cpp

--- 01_字符串/03_Automaton.cpp
+++ 01_字符串/03_Automaton.cpp
@@ -1,10 +1,11 @@
-class Automaton {
-public :
+struct Automaton {
+    static constexpr int ALPHABET_SIZE = 26;
     std::vector<std::vector<int>> tr;
     std::vector<int> e;
     std::vector<int> fail;
     int tot;
-    Automaton(int n) : tot(0), tr(n, std::vector<int>(26)), e(n), fail(n) {}
+
+    Automaton(int n) : tr(n, std::vector<int>(ALPHABET_SIZE)), e(n), fail(n), tot(0) {}
     Automaton(int m, std::vector<std::string> s) : Automaton(m) {
         for(int i = 0;i < (int) s.size(); i++) {
             insert(s[i]);
@@ -24,9 +25,9 @@ public :
     void build() {
         std::queue<int> q;
         for(int i = 0;i < 26; i++) {
-            if(tr[0][i]) q.push(tr[0][i]);
+            if(tr[0][i]) q.push(tr[0][i]);        
         }
-        while(!q.empty()) {
+        while(q.size()) {
             int u = q.front();
             q.pop();
             for(int i = 0;i < 26; i++) {
@@ -41,13 +42,13 @@ public :
     }
 
     int query(std::string t) {
-        int u = 0, ans = 0;
+        int u = 0, res = 0;
         for(int i = 0;i < (int) t.size(); i++) {
             u = tr[u][t[i] - 'a'];
             for(int j = u;j && e[j] != -1; j = fail[j]) {
-                ans += e[j], e[j] = -1;
+                res += e[j], e[j] = -1;
             }
         }
-        return ans;
+        return res;
     }
 };
