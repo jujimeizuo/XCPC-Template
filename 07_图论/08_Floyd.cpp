a4bc3f3c244aa48113c80ad86f76bf77b0048175
jujimeizuo <3058852685@qq.com>
2022-10-26 16:05:06 +0800

    update floyd

--- 07_图论/08_Floyd.cpp
+++ 07_图论/08_Floyd.cpp
@@ -1,8 +1,8 @@
 template <typename T>
 void floyd(std::vector<std::vector<T>>& dist) {
-    for (int k = 0; k < cost.size(); k++) {
-        for (int i = 0; i < cost.size(); i++) {
-            for (int j = 0; j < cost.size(); j++) {
+    for (int k = 0; k < dist.size(); k++) {
+        for (int i = 0; i < dist.size(); i++) {
+            for (int j = 0; j < dist.size(); j++) {
                 dist[i][j] = std::min(dist[i][j], dist[i][k], dist[k][j]);
             }
         }
